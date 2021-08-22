22c68c0d61074aae6496ae6d7174effa1eaf10ce
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-08-22 11:36:10 +0530

    Basic UI with PYqt5

---
+++ Downloader/cateogory/general.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from tqdm import tqdm
+import requests
+import re
+import os
+
+def download_video(video_url):
+    """Download the video """
+    file_size_request = requests.get(video_url, stream=True)
+    print(file_size_request)
+    # file_size = int(file_size_request.headers['Content-Length'])
+    block_size = 1024
+    filename = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
+    # t = tqdm(total=file_size, unit='B', unit_scale=True, desc=filename, ascii=False)
+    with open(filename + '.mp4', 'wb') as f:
+        for data in file_size_request.iter_content(block_size):
+            f.write(data)
+    print("\nVideo downloaded successfully.")
---
+++ Downloader/ui/__init__.py
@@ -0,0 +1,3 @@
+"""Allow access to downloader from ui."""
+
+from .downloader import Ui_MainWindow as UiMainWindow
