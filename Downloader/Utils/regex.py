99e9923bbab0a83d6fa4a7941e907982f5731e55
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-08-23 14:51:03 +0530

    Created a simple regex for torrents with magnetic links

--- Downloader/Utils/regex.py
+++ Downloader/Utils/regex.py
@@ -2,3 +2,5 @@ import re
 
 youtube_regex = re.compile(r'''(http:|https:)?\/\/(www\.)?(youtube.com|youtu.be)\/(watch)?(\?v=)?(\S+)?
         ''', re.VERBOSE)
+
+torrent_regex = re.compile(r'^(magnet:\?xt=urn:btih:).*')
