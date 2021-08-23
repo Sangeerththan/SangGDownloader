449c284d50db28c11dadd415a7cd16de0b6cbad6
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-08-23 11:28:55 +0530

    Shutdown after torrent download currently for UNIX

--- Downloader/cateogory/torrent_downlaod.py
+++ Downloader/cateogory/torrent_downlaod.py
@@ -30,21 +30,15 @@ def download_magnetic_link(_link, _path):
         time.sleep(5)
 
 
-def downloads(links, _path=BASE_PATH, shutdown='no'):
-    download_magnetic_link(
-        "magnet:?xt=urn:btih:3dfc016e786850cfe49dbe09f988aeeeb29437a5&dn=www.4MovieRulz.tc%20-%20THITTAM%20IRANDU%20(2021)%20Tamil%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20700MB%20-%20ESub.mkv&tr=udp%3a%2f%2ftracker.leechers-paradise.org%3a6969%2fannounce&tr=udp%3a%2f%2fp4p.arenabg.com%3a1337%2fannounce&tr=http%3a%2f%2fpow7.com%3a80%2fannounce&tr=udp%3a%2f%2f9.rarbg.me%3a2970%2fannounce&tr=udp%3a%2f%2f9.rarbg.com%3a2790%2fannounce&tr=udp%3a%2f%2f9.rarbg.to%3a2730%2fannounce&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.yoshi210.com%3a6969%2fannounce&tr=udp%3a%2f%2ftracker.tiny-vps.com%3a6969%2fannounce&tr=http%3a%2f%2ftracker2.itzmx.com%3a6961%2fannounce&tr=udp%3a%2f%2f151.80.120.114%3a2710%2fannounce&tr=http%3a%2f%2ftracker.yoshi210.com%3a6969%2fannounce&tr=udp%3a%2f%2fopen.stealth.si%3a80%2fannounce&tr=http%3a%2f%2ft.nyaatracker.com%3a80%2fannounce",
-        BASE_PATH)
-    download_magnetic_link(
-        "magnet:?xt=urn:btih:02422a0a98678f3e1560796e74b190d04c08a248&dn=www.4MovieRulz.lc%20-%20iSMART%20SHANKAR%20(2021)%20Tamil%20HQ%20HDRip%20-%20x264%20-%20Original%20Aud%20-%20700MB%20ESub.mkv&tr=udp%3a%2f%2ftracker.leechers-paradise.org%3a6969%2fannounce&tr=udp%3a%2f%2fp4p.arenabg.com%3a1337%2fannounce&tr=http%3a%2f%2fpow7.com%3a80%2fannounce&tr=udp%3a%2f%2f9.rarbg.me%3a2970%2fannounce&tr=udp%3a%2f%2f9.rarbg.com%3a2790%2fannounce&tr=udp%3a%2f%2f9.rarbg.to%3a2730%2fannounce&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.yoshi210.com%3a6969%2fannounce&tr=udp%3a%2f%2ftracker.tiny-vps.com%3a6969%2fannounce&tr=http%3a%2f%2ftracker2.itzmx.com%3a6961%2fannounce&tr=udp%3a%2f%2f151.80.120.114%3a2710%2fannounce&tr=http%3a%2f%2ftracker.yoshi210.com%3a6969%2fannounce&tr=udp%3a%2f%2fopen.stealth.si%3a80%2fannounce&tr=http%3a%2f%2ft.nyaatracker.com%3a80%2fannounce",
-        BASE_PATH)
-    shutdown(shutdown)
+def downloads(links, _path=BASE_PATH, _shutdown='no'):
+    shutdown(_shutdown)
 
 
 def shutdown(shut):
     if shut == 'no':
         exit()
     else:
-        os.system("shutdown /s /t 1")
+        os.system("systemctl poweroff")
 
 
 downloads([], '', 'yes')
