76a58634cd6872e8c63e12f865ebc617e268153d
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-08-24 23:36:32 +0530

    [CLEAN][DONE] - fb download logic modified

--- Downloader/Cateogory/fb.py
+++ Downloader/Cateogory/fb.py
@@ -9,15 +9,6 @@ from Downloader.Utils.tasks import shutdown
 
 def download(link):
     page = requests.get(link).content.decode('utf-8')
-    _qualityhd = re.search('hd_src:"https', page)
-    _qualitysd = re.search('sd_src:"https', page)
-    _hd = re.search('hd_src:null', page)
-    _sd = re.search('sd_src:null', page)
-    list = []
-    _thelist = [_qualityhd, _qualitysd, _hd, _sd]
-    for id, val in enumerate(_thelist):
-        if val != None:
-            list.append(id)
     resolutions = ['hd', 'sd']
     video_url = ''
     for resolution in resolutions:
