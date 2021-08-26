d63d5f7814509e140b8be98f1b4218e9ec1dbca3
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-08-27 00:24:52 +0530

    [CONFIG][PENDING] - Basic setup file for PyPI distributions.

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="San G Downloader",
+    version="0.1",
+    author="Sangeerththan Balachandran",
+    author_email="sangeerth20@gmail.com",
+    description="Downloader for streams")
