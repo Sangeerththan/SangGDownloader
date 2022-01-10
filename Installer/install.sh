35bdaea2eaacf001e5f926fd779215a69d3839bd
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2022-01-10 22:08:37 +0530

    [CONFIG][UPDATE] - Packaging commands are optional.

--- Installer/install.sh
+++ Installer/install.sh
@@ -7,7 +7,10 @@ sudo apt-get install python3-tk
 
 echo "Installation completed"
 
+#Below steps are for packaging your application
+#Execute it if you want to build an executable or a wheel
+
 #pip install pyinstaller
-pyinstaller main.py
-cd dist/main
-./main
+#pyinstaller main.py
+#cd dist/main
+#./main
