45764a6c93000c24a25b1a458b1057a088438836
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2022-01-10 22:13:36 +0530

    [REFACTOR][DONE] - pytube implementation removed.

--- Downloader/Cateogory/youtube.py
+++ Downloader/Cateogory/youtube.py
@@ -1,9 +1,7 @@
 from __future__ import unicode_literals
 import datetime
 import time
-import pytube
 import youtube_dl
-from Downloader.Utils.file_operations import progress_function
 from Downloader.Utils.tasks import shutdown
 from Downloader.configuration import YOUTUBE_PATH
 from Downloader.Utils.file_operations import create_folder
@@ -16,15 +14,9 @@ def download_youtube_videos(_links, _shutdown='no', _path=YOUTUBE_PATH):
     count = 0
     for link in _links:
         count += 1
-        # yt = pytube.YouTube(link, on_progress_callback=progress_function)
-        # for resolution in resolutions:
-        #     stream = yt.streams.filter(progressive=True, res=resolution).first()
-        #     if stream is not None:
-        #         break;
         start_time = time.time()
         print("Start Time for Download Link " + str(count) + ": " + link + " " + str(datetime.datetime.now()))
         print("Download Started ".center(50, "*"))
-        # stream.download(output_path=_path)
         ydl_opts = {
             'outtmpl': os.path.join(_path, '%(title)s.%(ext)s')
         }
